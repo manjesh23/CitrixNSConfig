80c3bece763e7d1a2491e8a7c69d9d606c4dfdb2
Manjesh Setty <manjesh_n@hotmail.com>
2022-05-23 19:03:33 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
-function openPage(pageName, elmnt, color) {
+function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablink");
+    tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(pageName).style.display = "block";
-    elmnt.style.backgroundColor = color;
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+
+function analyze() {
+    let nsconf = document.getElementById('nsconf').files[0]
+    let reader = new FileReader()
+    reader.onload = function() {
+        let rawnsconfig = this.result.split("/r/n");
+        for (var line = 0; line < rawnsconfig.length; line++) {
+            let linenscount = rawnsconfig[line]
+            if (linenscount.length > 2) {
+                //rawnsconfig output here //
+                document.getElementById("nsconfigout").style.display = 'block'
+                document.getElementById("nsconfigout").innerText = rawnsconfig
+                    //basicnsconfig output here
+                document.getElementById("basicnsconfigout").style.display = 'block'
+                document.getElementById("basicnsconfigout").innerText = rawnsconfig.match("/Build/g")
+            } else {
+                alert("Invalid ns.conf file")
+            }
+        }
+    }
+    reader.readAsText(nsconf)
+    reader.onerror = (event) => {
+        alert(event.target.error.name);
+    }
+}
