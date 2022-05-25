203eceecae0c57cf479ee79b4a57f3f0d9da07de
Manjesh Setty <manjesh_n@hotmail.com>
2022-05-25 21:32:12 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -12,21 +12,50 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
-
 function analyze() {
+    document.getElementById("nsconfmenu").style.display = 'block'
     let nsconf = document.getElementById('nsconf').files[0]
     let reader = new FileReader()
     reader.onload = function() {
-        let rawnsconfig = this.result.split("/r/n");
+        let rawnsconfig = this.result.split("/r/n")
         for (var line = 0; line < rawnsconfig.length; line++) {
-            let linenscount = rawnsconfig[line]
-            if (linenscount.length > 2) {
+            let originalnsconf = rawnsconfig[line]
+            if (originalnsconf.length > 2) {
                 //rawnsconfig output here //
-                document.getElementById("nsconfigout").style.display = 'block'
-                document.getElementById("nsconfigout").innerText = rawnsconfig
+                document.getElementById("nsconfigout").innerText = originalnsconf
                     //basicnsconfig output here
-                document.getElementById("basicnsconfigout").style.display = 'block'
-                document.getElementById("basicnsconfigout").innerText = rawnsconfig.match("/Build/g")
+                    //system details
+                document.getElementById("nsversion").innerText = originalnsconf.match(/NS.+Build.+/g)[0]
+                document.getElementById("nsip").innerText = originalnsconf.match(/ns\sconfig\s.IPAddress.+/g)[0].split(" ")[3] + " / " + originalnsconf.match(/ns\sconfig\s.IPAddress.+/g)[0].split(" ")[5]
+                document.getElementById("nsfeatures").innerText = originalnsconf.match(/\sfeature.+/g)[0].split(" ").slice(2)
+                document.getElementById("nsmode").innerText = originalnsconf.match(/ns\smode.+/g)[0].split(" ").slice(2)
+                document.getElementById("nshostname").innerText = originalnsconf.match(/ns\shostName.+/g)[0].split(" ")[2]
+                if (originalnsconf.match(/HA\snode.+/g) != null) {
+                    document.getElementById("nshanode").innerText = originalnsconf.match(/HA\snode.+/g)[0].split(" ")[3]
+                }
+                document.getElementById("nstimezone").innerText = originalnsconf.match(/-timezone.+/g)[0].split(" ")[1]
+                    //load balancing realted details
+                    // loadbalancing virtual server info
+                let rawlbvserver = originalnsconf.match(/add\slb\svserver.+/g)
+                if (rawlbvserver.length > 0) {
+                    var lbvservertable = ""
+                    for (var lbvserver = 0; lbvserver < rawlbvserver.length; lbvserver++) {
+                        let lbpersistence = rawlbvserver[lbvserver].match(new RegExp("persistenceType" + '\\s(\\w+)'))
+                        if (lbpersistence != null) {
+                            lbpersistence = lbpersistence[1]
+                        }
+                        let lblbmethod = rawlbvserver[lbvserver].match(new RegExp("lbMethod" + '\\s(\\w+)'))
+                        if (lblbmethod != null) {
+                            lblbmethod = lblbmethod[1]
+                        }
+                        let lbbackupLBMethod = rawlbvserver[lbvserver].match(new RegExp("backupLBMethod" + '\\s(\\w+)'))
+                        if (lbbackupLBMethod != null) {
+                            lbbackupLBMethod = lbbackupLBMethod[1]
+                        }
+                        lbvservertable += rawlbvserver[lbvserver].split(" ")[3] + "</td><td>" + rawlbvserver[lbvserver].split(" ")[4] + "</td><td>" + rawlbvserver[lbvserver].split(" ")[5] + "</td><td>" + rawlbvserver[lbvserver].split(" ")[6] + "</td><td>" + lbpersistence + "</td><td>" + lblbmethod + "</td><td>" + lbbackupLBMethod + "</td></tr><td>"
+                    }
+                    document.getElementById("lbvserver").innerHTML += "<br><br><table><tr><th>LB vServer Name</th><th>Protocol</th><th>LB VIP</th><th>VIP Port</th><th>persistenceType</th><th>lbMethod</th><th>backupLBMethod</th></tr><tr><td>" + lbvservertable + "</table>"
+                }
             } else {
                 alert("Invalid ns.conf file")
             }
