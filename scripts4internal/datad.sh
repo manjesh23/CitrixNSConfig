412bf0a1723ae3906d0b87694c5b6f0758032b9c
Manjesh N <manjesh_n@hotmail.com>
2024-09-11 22:19:59 +0530

    commit

--- scripts4internal/datad.sh
+++ scripts4internal/datad.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/bash
 caseformat="^[0-9]{8}"
 url="https://tooltrack.deva.citrite.net/use/conFetch"
-ver="2.1"
+ver="2.4"
 datad="datad--$1--$(date +%s)"
 s="Success"
 f="Failed"
@@ -14,7 +14,9 @@ if [[ $1 =~ $caseformat ]]; then
     cd $(echo /upload/ftp/$1) >/dev/null 2>&1
     if [ $? -eq 0 ]; then
         # Find and print all the collector bundles of NetScaler (Including nested directories)
-        find ./ -type d -name "collector_*" -exec ls -ld {} + | cut -d: -f2- | cut -c 6- | sed 's/ /\\ /g' | sed 's/(/\\(/g' | sed 's/)/\\)/g' | awk 'BEGIN{printf "\n\t%s\n\n", "\033[1;97mCollector_Bundles\033[0m"}!/tar|gz/{printf "%s%s\n", "\033[36m","./"$0"\033[0m"}'
+        [ "$(find ./ -type d -name 'collector_*')" ] && find ./ -type d -name "collector_*" -exec ls -ld {} + | cut -d: -f2- | cut -c 6- | sed 's/ /\\ /g' | sed 's/(/\\(/g' | sed 's/)/\\)/g' | awk 'BEGIN{printf "\n\t%s\n\n", "\033[1;97mCollector_Bundles\033[0m"}!/tar|gz/{printf "%s%s\n", "\033[36m","./"$0"\033[0m"}'
+        [ "$(find ./ -type d -name 'NetScaler_ADM_*mps')" ] && find ./ -type d -name "NetScaler_ADM_*mps" -exec ls -ld {} + | cut -d: -f2- | cut -c 6- | sed 's/ /\\ /g' | sed 's/(/\\(/g' | sed 's/)/\\)/g' | awk 'BEGIN{printf "\n\t%s\n\n", "\033[1;97mNetScaler_ADM_Bundles\033[0m"}!/tar|gz/{printf "%s%s\n", "\033[36m","./"$0"\033[0m"}'
+        [ "$(find ./ -type d -name 'Citrix_ADM_*mps')" ] && find ./ -type d -name "Citrix_ADM_*mps" -exec ls -ld {} + | cut -d: -f2- | cut -c 6- | sed 's/ /\\ /g' | sed 's/(/\\(/g' | sed 's/)/\\)/g' | awk 'BEGIN{printf "\n\t%s\n\n", "\033[1;97mCitrix_ADM_Bundles\033[0m"}!/tar|gz/{printf "%s%s\n", "\033[36m","./"$0"\033[0m"}'
         echo -e "\033[5;33m\nCheck out the new features! Use \`show --help\` to learn more.\033[0m"
         # Write the datad usage to tooltrack
         screen -dmS "tooltrack" curl -k --silent --data $successq $url >/dev/null 2>&1
